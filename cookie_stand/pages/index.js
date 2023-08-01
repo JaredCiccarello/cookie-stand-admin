75add8f9ec02e7b42269a6f550d523e52c843d46
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-08-01 18:53:29 -0400

    CSS almost there

--- cookie_stand/pages/index.js
+++ cookie_stand/pages/index.js
@@ -1,59 +1,45 @@
 import Head from 'next/head';
-import { useState } from "react";
 
-export default function Home(){
+export default function Home() {
   return (
     <div>
       <Head>
-        <title>Cookie Stand Admin</title>
+        <title>Cookie Stand</title>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.0/tailwind.min.css" rel="stylesheet" />
       </Head>
       <header class="bg-green-500 text-white p-4">
         <h1 class="text-3xl">Cookie Stand Admin</h1>
       </header>
-      <body>
-      
-          <main class="bg-green-100 p-4">
-            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-              <h1 class="text-2xl">Create Cookie Stand</h1>
-              <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="location">
-                  Location
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-              </div>
-              <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="minimum-customers-per-hour">
-                  
-                  Minimum Customers per Hour
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-              </div>
-              <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="maximum-customers-per-hour">
+      <h1 className="mb-4 text-3xl">Cookie Stand Admin</h1>
 
-                  Maximum Customers per Hour
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-              </div>
-              <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="average-cookies-per-sale">
+      <div className="container mx-auto">
+        <h2 className="mb-4 text-xl">Create Cookie Stand</h2>
+        <form action="/create" method="post" className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          <div className="sm:col-span-5">
+            <label htmlFor="location" className="block mb-2">Location</label>
+            <input type="text" name="location" id="location" className="w-full px-4 py-2 form-control" />
+          </div>
+          <div className="flex flex-col sm:col-span-1">
+            <label htmlFor="min_customers_per_hour" className="block w-full mb-2 text-center">Minimum Customers per Hour</label>
+            <input type="number" name="min_customers_per_hour" id="min_customers_per_hour" className="form-control" />
+          </div>
+          <div className="flex flex-col sm:col-span-1">
+            <label htmlFor="max_customers_per_hour" className="block w-full mb-2 text-center">Maximum Customers per Hour</label>
+            <input type="number" name="max_customers_per_hour" id="max_customers_per_hour" className="form-control" />
+          </div>
+          <div className="flex flex-col sm:col-span-1">
+            <label htmlFor="average_cookies_per_sale" className="block w-full mb-2 text-center">Average Cookies per Sale</label>
+            <input type="number" name="average_cookies_per_sale" id="average_cookies_per_sale" className="form-control" />
+          </div>
+          <div className="flex items-center justify-center sm:col-span-2">
+            <button type="submit" className="w-full px-4 py-2 btn btn-primary">Create</button>
+          </div>
 
-                  Average Cookies per Sale
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-              </div>
-              <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                Create
-              </button>
-            </form>
-          </main>
-        
-      </body>
-      <footer class="bg-green-500 text-white p-4 fixed bottom-0 w-full">
+          <footer class="bg-green-500 text-white p-4 fixed bottom-0 w-full">
         <p>Jared Ciccarello &copy; 2023</p>
       </footer>
-      
+        </form>
+      </div>
     </div>
-  )
+  );
 }
